3e237cdef6be1d81d9d144e37f291abe55de3c9c
audaz799 <matiashuancagato@gmail.com>
2024-09-14 12:01:36 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+PALABRA=imput("que palabra quieres aprender : " )
+meme_dict = ("cringe":"pena ajena" "gg" "good game")
+npc = no playable character
+brb = be right back 
+idk = i dont know
+zzz = dormido 
+fps = fotogramas por segundos
+xp = experencie point 
+noob = novato 
+hitbox = hit box 
+imput = palabra que quieres aprender
+
+if palabra in meme_dict.keys():
+print(memedict[palabra]
+else
+print("esa palabra no la tenemos")
